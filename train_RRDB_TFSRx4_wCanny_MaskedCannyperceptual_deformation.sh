1c1ebc5e4addabd75a00c559480fd528ff6da693
mjq11302010044 <mjq11302010044@github.com>
2023-08-22 17:09:28 +0800

    new

--- train_RRDB_TFSRx4_wCanny_MaskedCannyperceptual_deformation.sh
+++
@@ -1,3 +0,0 @@
-PYTHONPATH="./:${PYTHONPATH}" \
-	CUDA_VISIBLE_DEVICES=4 \
-	python basicsr/train.py -opt options/train/ESRGAN/train_RRDBNet_PSNR_x4_TFSR_wCanny_MaskedCannyperceptual_deformation.yml
